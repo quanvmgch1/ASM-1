d40b05d1091f46f33bd7fdd88d88189595af5a86
quanvmgch17620 <quanvmgch17620@fpt.edu.vm>
2020-03-12 14:12:53 +0700

    quanvm

---
+++ myserver.js
@@ -0,0 +1,16 @@
+var express=require('express');
+var app=express();
+
+app.set('view engine','jade');
+var port = process.env.PORT || 4000;
+
+var publicDir = require('path').join(__dirname,'/public');
+app.use(express.static(publicDir));
+
+
+//khi mo website mac dinh
+app.get("/",(req,res)=>{
+    res.render('shop');
+})
+
+var server=app.listen(port,function() {});
